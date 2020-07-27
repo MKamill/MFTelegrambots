abad3ce8c2963aa690c4473cf7fc0c8ad44a2bd1
MKamill <mirgayazow2014@yandex.ru>
2020-07-27 22:36:55 +0500

    elif added

--- first_telegram_bots.py
+++ first_telegram_bots.py
@@ -7,6 +7,8 @@ bot = telebot.TeleBot( '1092838389:AAFc8XHNwhOyWVRKD2-nRTPUCFspbuzev0k')
 def send_msg(message):
     if message.text == 'Карим':
         bot.send_message(message.chat.id, "лошара")
+    elif message.text == 'Камиль':
+        bot.send_message(message.chat.id, "крутой")
     else:
         bot.send_message(message.chat.id, message.text)
 
