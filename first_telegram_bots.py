a13d971195e0314cbda44e8a4dab51502ab76dfe
MKamill <mirgayazow2014@yandex.ru>
2020-07-27 22:11:22 +0500

    Initial commit

---
+++ first_telegram_bots.py
@@ -0,0 +1,14 @@
+import telebot
+
+bot = telebot.TeleBot( '1092838389:AAFc8XHNwhOyWVRKD2-nRTPUCFspbuzev0k')
+
+
+@bot.message_handler(content_types=['text'])
+def send_msg(message):
+    if message.text == 'Карим':
+        bot.send_message(message.chat.id, "лошара")
+    else:
+        bot.send_message(message.chat.id, message.text)
+
+
+bot.polling(none_stop=True)
