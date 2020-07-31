94eda52acd89d77c64ab1f231e0d1b8e8a937abc
MKamill <mirgayazow2014@yandex.ru>
2020-07-31 15:55:22 +0500

    Удалил все не нужное, оставил голую инициализацию с функцией эхо

--- first_telegram_bots.py
+++ first_telegram_bots.py
@@ -1,21 +1,14 @@
 import telebot
-import work_with_email as email
-import defsForAnalysis as analysis
 
-bot = telebot.TeleBot('1092838389:AAFc8XHNwhOyWVRKD2-nRTPUCFspbuzev0k')
+f = open('bot_config', 'r')
+Token = f.read()
+
+bot = telebot.TeleBot(token=Token)
 
 
 @bot.message_handler(content_types=['text'])
 def send_msg(message):
-    if analysis.call_all_defs(message.text):
-        email.the_process_of_sending('2:45')
-
-    if message.text == 'Карим':
-        bot.send_message(message.chat.id, "лошара_aaaaaaaaaaaaaaaaaaa")
-    elif message.text == 'Камиль':
-        bot.send_message(message.chat.id, "крутой")
-    else:
-        bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, message.text)
 
 
 bot.polling(none_stop=True)
