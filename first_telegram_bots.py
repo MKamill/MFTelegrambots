4d843568dd8756e3e37a5e6f01deb2e0899d7baa
MKamill <mirgayazow2014@yandex.ru>
2020-07-31 17:36:06 +0500

    test5

--- first_telegram_bots.py
+++ first_telegram_bots.py
@@ -1,16 +1,15 @@
 import telebot
 
 
-def enable_bot():
-    f = open(r'/storage/emulated/0/Android/data/ru.iiec.pydroid3/files/bot_config.txt', 'r')
-    Token = f.read()
+f = open(r'/storage/emulated/0/Android/data/ru.iiec.pydroid3/files/bot_config.txt', 'r')
+Token = f.read()
 
-    bot = telebot.TeleBot(Token)
+bot = telebot.TeleBot('1092838389:AAFc8XHNwhOyWVRKD2-nRTPUCFspbuzev0k')
 
-    # 11 дай уйти
+# 11 дай уйти
 
-    @bot.message_handler(content_types=['text'])
-    def send_msg(message):
-        bot.send_message(message.chat.id, message.text)
+@bot.message_handler(content_types=['text'])
+def send_msg(message):
+    bot.send_message(message.chat.id, message.text)
 
-    bot.polling(none_stop=True)
+bot.polling(none_stop=True)
