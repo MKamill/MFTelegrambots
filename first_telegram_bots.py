ae56fd2b4b1dfb4b2d2ea117babec60a245d7453
MKamill <mirgayazow2014@yandex.ru>
2020-07-31 19:46:23 +0500

    авторизация fixed1

--- first_telegram_bots.py
+++ first_telegram_bots.py
@@ -5,16 +5,21 @@ Token = f.read()
 
 bot = telebot.TeleBot(Token)
 
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Здравствуйте, введите ваш пароль:')
 
+
 @bot.message_handler(content_types=['text'])
 def send_msg(message):
-    if message =='0909':
+    if message == '0909':
         bot.send_message(message.chat.id, 'Приветствуем Вас Юлия')
-    elif message=='9090':
+    elif message == '9090':
         bot.send_message(message.chat.id, 'Приветствуем Вас Камиль')
+    else:
+        bot.send_message(message.chat.id, message.text)
+
 
 
 
