104fc1cece1fc986b51d0f84de5f7ba54517b4c3
MKamill <mirgayazow2014@yandex.ru>
2020-07-31 22:50:24 +0500

    реализованы кнопки

--- first_telegram_bots.py
+++ first_telegram_bots.py
@@ -1,3 +1,5 @@
+import types
+
 import telebot
 import time
 
@@ -19,19 +21,39 @@ def start_message(message):
 def send_msg(message):
     if message.text == '0909':
         bot.send_message(message.chat.id, 'Приветствуем Вас Юлия')
+        keyboard = types.InlineKeyboardMarkup()
+
+        key_deva = types.InlineKeyboardButton(text='Дева', callback_data='zodiac')
+
+        keyboard.add(key_deva)
+
+        key_vesy = types.InlineKeyboardButton(text='Весы', callback_data='zodiac')
+
+        keyboard.add(key_vesy)
+
+        key_scorpion = types.InlineKeyboardButton(text='Скорпион', callback_data='zodiac')
+
+        bot.send_message(message.from_user.id, text='Выбери свой знак зодиака', reply_markup=keyboard)
     elif message.text == '9090':
         bot.send_message(message.chat.id, 'Приветствуем Вас Камиль')
     else:
         bot.send_message(message.chat.id, message.text)
 
 
+@bot.callback_query_handler(func=lambda call: True)
+
+def callback_worker(call):
+    if call.data == "zodiac":
+        msg='вы нажали на кнопку'
+        bot.send_message(call.message.chat.id, msg)
+
 while True:
     try:
         bot.polling(none_stop=True)
     except:
         local_time = float(1)
-        local_time = local_time * 60
+        local_time = local_time * 10
         time.sleep(local_time)
         disconnect_counter += 1
-        print('----------------------[' + str(disconnect_counter) + ']----------------------')
+        print('----------------------[' + str(disconnect_counter/60) + ']----------------------')
         continue
