e74a1c11f9f863683bde40dc53c745f141ada450
MKamill <mirgayazow2014@yandex.ru>
2020-08-09 22:00:49 +0500

    return added

--- work_with_text_type.py
+++ work_with_text_type.py
@@ -2,16 +2,16 @@ import sqlite3 as lite
 import work_with_connection as connection
 
 
-@connection.bot.message_handler(content_types=["text"])
-def start_message(message):
-    try:
-        connection.bot.send_message(message.chat.id, message.text)
-    except:
-        connection.bot.send_message(message.chat.id, 'пробую заново')
-        try:
-            start_message(message)
-        except:
-            connection.bot.send_message(message.chat.id, 'рекурсивная ошибка')
+# @connection.bot.message_handler(content_types=["text"])
+# def start_message(message):
+#     try:
+#         connection.bot.send_message(message.chat.id, message.text)
+#     except:
+#         connection.bot.send_message(message.chat.id, 'пробую заново')
+#         try:
+#             start_message(message)
+#         except:
+#             connection.bot.send_message(message.chat.id, 'рекурсивная ошибка')
 
 
 def main_search(ch_id):
@@ -28,20 +28,11 @@ def main_search(ch_id):
             path = cursor.fetchone()
             doc = open(fr'{path}'.replace('(', '').replace(')', '').replace('\'', '').replace(',', ''), 'rb')
             connection.bot.send_photo(message.chat.id, doc)
+            return
         except:
             try:
                 search_photo(message)
+                return
             except:
                 connection.bot.send_message(message.chat.id, 'рекурсивная ошибка')
-
-
-@connection.bot.message_handler(content_types=["text"])
-def start_message(message):
-    try:
-        connection.bot.send_message(message.chat.id, message.text)
-    except:
-        connection.bot.send_message(message.chat.id, 'пробую заново')
-        try:
-            start_message(message)
-        except:
-            connection.bot.send_message(message.chat.id, 'рекурсивная ошибка')
+                return
