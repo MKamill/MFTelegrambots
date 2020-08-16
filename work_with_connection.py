9d076b55eb8849178a8f6ad5f55dc76c6ca08460
MKamill <mirgayazow2014@yandex.ru>
2020-08-16 16:01:34 +0500

    subprocess

--- work_with_connection.py
+++ work_with_connection.py
@@ -1,6 +1,12 @@
 import telebot
+import subprocess
+import work_with_connection as connection
 
 f = open(r'/data/data/com.termux/files/home/telegram/bot_config.txt', 'r')
 Token = f.read()
 bot = telebot.TeleBot(Token)
-#2
+
+
+@connection.bot.message_handler(commands=['reboot'])
+def start_message(message):
+    subprocess.call(['./reboot.sh'])
